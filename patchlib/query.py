stefanha/patches

commit 8feb88401a27e1238b5b81265bff36a4e4a19138

list: make default list output configurable
Signed-off-by: Anthony Liguori <aliguori@us.ibm.com>

patchlib/query.py
@@ -117,6 +117,9 @@ def eval_query_term(series, term, scope):
     else:
         command = None
 
+    if command == None:
+        raise Exception('foo')
+
     if command == 'status':
         status = args.lower()
 
@@ -128,12 +131,6 @@ def eval_query_term(series, term, scope):
             return is_pull_request(series)
         elif status == 'rfc':
             return is_rfc(series)
-        elif status == 'unapplied':
-            return not (is_broken(series) or
-                       is_obsolete(series) or
-                       is_pull_request(series) or
-                       is_rfc(series) or
-                       is_committed(series))
         elif status == 'committed':
             return is_committed(series)
         elif status == 'reviewed':
@@ -142,7 +139,8 @@ def eval_query_term(series, term, scope):
             raise Exception("Unknown status `%s'" % status)
     elif command == 'label':
         txt = config.get_label(args)
-        t, _ = parse_query(tokenize_query(txt))
+        tks = tokenize_query(txt)
+        t, _ = parse_query(tks)
         return eval_query(series, t, scope)
     elif command == 'from':
         def fn(msg):
@@ -168,7 +166,7 @@ def eval_query_term(series, term, scope):
         def fn(msg):
             return msg['message-id'] == args
         return eval_messages(series, fn, scope)
-    elif command.startswith('buildbot['):
+    elif command and command.startswith('buildbot['):
         name = command[9:].split(']', 1)[0]
         if 'buildbots' in series and name in series['buildbots']:
             bot = series['buildbots'][name]

patches
@@ -90,7 +90,7 @@ def main(args):
 
     subparser = subparsers.add_parser('list',
                                       help='List pending patches')
-    subparser.add_argument('query', type=str, nargs='*', default=['status:unapplied'], help='Search query')
+    subparser.add_argument('query', type=str, nargs='*', default=['label:__list__'], help='Search query')
     subparser.add_argument('--format', type=str, default='full', help='Select output format (default "oneline")')
     subparser.set_defaults(func=do_list)
 

patchlib/config.py
@@ -77,7 +77,7 @@ def get_links():
     return ret
 
 def get_label(label):
-    return ini.get('labels', label)
+    return get('labels.%s' % label)
 
 def get_notifications():
     ret = []
@@ -127,6 +127,9 @@ def get(key):
         value = get_patches_dir() + '/buildbot.json'
     elif key == 'buildbot.owner':
         value = get_default_sender()
+    elif key == 'labels.__list__':
+        value = ('not status:broken not status:obsolete ' +
+                 'not status:pull-request not status:rfc not status:committed)')
     else:
         value = None
 

commit 7b928766448090b6704f99e3bec239f17ad404f5

query: bring back status:unapplied
Commit 8feb88401a27e1238b5b81265bff36a4e4a19138 ("list: make default
list output configurable") accidentally dropped status:unapplied.

Bring back status:unapplied, which is still documented in README.md.

Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

patchlib/query.py
@@ -137,6 +137,12 @@ def eval_query_term(series, term, scope):
             return is_pull_request(series)
         elif status == 'rfc':
             return is_rfc(series)
+        elif status == 'unapplied':
+            return not (is_broken(series) or
+                       is_obsolete(series) or
+                       is_pull_request(series) or
+                       is_rfc(series) or
+                       is_committed(series))
         elif status == 'committed':
             return is_committed(series)
         elif status == 'reviewed':